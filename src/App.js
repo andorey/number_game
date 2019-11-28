c0b1ecd87e7ef6c7cbbf756d3fca4ff239f93416
andoreyrich <andoreyrich@gmail.com>
2019-11-28 23:41:38 +0100

    create structure html view

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends React.Component {
 
-      </header>
-    </div>
-  );
+    state = {
+        disabled: false,
+
+    }
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1>Guess the Number</h1>
+                    <input type="button" 
+						   disabled={this.state.disabled} 
+						   value={'To Generate a Random Number'} />
+					<input type="text"
+						   placeholder={'Enter your number'}/>
+					<input type="button"
+						   disabled={this.state.disabled}
+						   value={'Check your number'} />
+
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
