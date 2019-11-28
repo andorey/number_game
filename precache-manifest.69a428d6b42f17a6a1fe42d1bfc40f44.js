f71c1fb5dfb67663caed2d8e856549393d6a328e
andoreyrich <andoreyrich@gmail.com>
2019-11-29 00:06:00 +0100

    Updates

---
+++ precache-manifest.69a428d6b42f17a6a1fe42d1bfc40f44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeec0e84bd9d6f40162cd78533114eb0",
+    "url": "/number_game/index.html"
+  },
+  {
+    "revision": "3ef5e4f1eeb3df311b9a",
+    "url": "/number_game/static/css/main.f49dd15b.chunk.css"
+  },
+  {
+    "revision": "63effe061bb09b1d17c9",
+    "url": "/number_game/static/js/2.d1400f1a.chunk.js"
+  },
+  {
+    "revision": "3ef5e4f1eeb3df311b9a",
+    "url": "/number_game/static/js/main.fb06df47.chunk.js"
+  },
+  {
+    "revision": "9ba3cdb2a29c4c9ede61",
+    "url": "/number_game/static/js/runtime-main.168aee27.js"
+  }
+]);
