dfc0ce306f6d94ae3036d25ee8f21db6b488dda0
andoreyrich <andoreyrich@gmail.com>
2019-11-29 00:47:43 +0100

    added hidden property for <div Counter>

--- src/App.js
+++ src/App.js
@@ -4,23 +4,46 @@ import './App.css';
 class App extends React.Component {
 
     state = {
-        disabled: false,
+        checker: true,
+		starter: false,
+		num: 0,
+		countdown: 120,
+		countHid: true
+    };
 
-    }
+	makeNumber = () => {
+		this.setState({
+			checker: false,
+			num:  Math.ceil(Math.random() * 100),
+			countHid: false,
+			starter: true
+		});
+	};
+
+	//setInterval(timer, 1000);
+
 
 	render() {
 		return (
 			<div className="App">
 				<header className="App-header">
 					<h2>Guess the Number</h2>
+					<i className="Counter"
+						 hidden={this.state.countHid}>
+						You have {this.state.countdown} sec.
+					</i>
 					<div className="wrapper">
-                    <input type="button" 
-						   disabled={this.state.disabled} 
-						   value={'To Generate a Random Number'} />
-					<input type="text"
-						   placeholder={'Enter your number'}/>
-					<input type="button"
-						   disabled={this.state.disabled}
+                    <input className="Starter"
+						   type="button"
+						   disabled={this.state.starter}
+						   value={'To Generate a Random Number'}
+						   onClick={this.makeNumber} />
+					<input className="Window"
+						   type="number" min="0" max="100"
+						   placeholder={'Enter your number'} />
+					<input className="Checker"
+						   type="button"
+						   disabled={this.state.checker}
 						   value={'Check your number'} />
 					</div>
 					<div className="Display">result guess</div>
