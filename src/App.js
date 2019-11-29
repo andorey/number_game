ee8e7643d014cff7bb36c5866dc9f873ba6eaa82
andoreyrich <andoreyrich@gmail.com>
2019-11-29 02:07:08 +0100

    fixing error in browser

--- src/App.js
+++ src/App.js
@@ -1,56 +1,1 @@
-import React from 'react';
-import './App.css';
-
-class App extends React.Component {
-
-    state = {
-        checker: true,
-		starter: false,
-		num: 0,
-		countdown: 120,
-		countHid: true
-    };
-
-	makeNumber = () => {
-		this.setState({
-			checker: false,
-			num:  Math.ceil(Math.random() * 100),
-			countHid: false,
-			starter: true
-		});
-	};
-
-	//setInterval(timer, 1000);
-
-
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<h2>Guess the Number</h2>
-					<i className="Counter"
-						 hidden={this.state.countHid}>
-						You have {this.state.countdown} sec.
-					</i>
-					<div className="wrapper">
-                    <input className="Starter"
-						   type="button"
-						   disabled={this.state.starter}
-						   value={'To Generate a Random Number'}
-						   onClick={this.makeNumber} />
-					<input className="Window"
-						   type="number" min="0" max="100"
-						   placeholder={'Enter your number'} />
-					<input className="Checker"
-						   type="button"
-						   disabled={this.state.checker}
-						   value={'Check your number'} />
-					</div>
-					<div className="Display">result guess</div>
-				</header>
-			</div>
-		);
-	}
-}
-
-export default App;
+import React from 'react';import './App.css';class App extends React.Component {	state = {		checker: true,		starter: false,		num: 0,		numInput: 0,		countdown: 120,		countHid: true	};	makeNumber = () => {		this.setState({			checker: false,			num:  Math.ceil(Math.random() * 100),			countHid: false,			starter: true		});	};	changeInputNum = (e) => {		this.inputNum(e.currentTarget.value);	};	inputNum = (value) => {		this.setState({numInput: value});	};	//setInterval(timer, 1000);	render() {		return (			<div className="App">				<header className="App-header">					<h2>Guess the Number</h2>					<i className="Counter"					   hidden={this.state.countHid}>						You have {this.state.countdown} sec.					</i>					<div className="wrapper">						<input className="Starter"							   type="button"							   disabled={this.state.starter}							   value={'To Generate a Random Number'}							   onClick={this.makeNumber} />						<input className="Window"							   type="number" min="0" max="100"							   placeholder={'Enter your number'}							   value={this.state.numInput}							   onChange={this.changeInputNum} />						<input className="Checker"							   type="button"							   disabled={this.state.checker}							   value={'Check your number'} />					</div>					<div className="Display">result guess</div>				</header>			</div>		);	}}export default App;
